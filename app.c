7348ff0b2b34df13bf80d0e21ea33b0be16ee1ca
AlCalzone <dominic.griesel@nabucasa.com>
2026-08-11 14:37:37 +0200

    Reconcile persisted Z-Wave identity during startup (#5)

--- app.c
+++ app.c
@@ -34,6 +34,7 @@
 #include "cc_color_switch_config_api.h"
 #include "cc_color_switch_io.h"
 #include "board_indicator_control.h"
+#include "zwave_identity.h"
 
 #ifdef SL_CATALOG_ZW_CLI_COMMON_PRESENT
 #include "zw_cli_common.h"
@@ -116,6 +117,12 @@ ZW_APPLICATION_STATUS ApplicationInit(__attribute__((unused)) zpal_reset_reason_
     if (nvm_init_done) {
       RepeaterConfigWriteDefaults(RadioConfig);
     }
+  }
+
+  // Repair/Reconcile the S2 identity before the protocol validates it.
+  if (!zwave_identity_reconcile(ZAF_isLongRangeRegion(RadioConfig->eRegion))) {
+    bootloader_rebootAndInstall();
+    return APPLICATION_POWER_DOWN;
   }
 
   /*
